4baba871c71cd2e3a570bd7680a38a81042c487f
Esko Luontola <esko@luontola.fi>
2021-08-02 20:16:35 +0300

    Rotating 3x3 shape, initial orientation

---
+++ test/RotatingShapes.test.mjs
@@ -0,0 +1,35 @@
+import {expect} from "chai";
+
+class RotatingShape {
+  #shape;
+
+  constructor(shape) {
+    this.#shape = shape
+      .replaceAll(" ", "")
+      .split("\n")
+      .map(row => row.split(""))
+  }
+
+  toString() {
+    return this.#shape
+      .map(row => row.join(""))
+      .join("\n") + "\n";
+  }
+}
+
+describe("Rotating 3x3 shape", () => {
+
+  it("initial orientation", () => {
+      const shape = new RotatingShape(
+        `ABC
+         DEF
+         GHI`
+      );
+      expect(shape.toString()).to.equalShape(
+        `ABC
+         DEF
+         GHI`
+      );
+    }
+  )
+})
