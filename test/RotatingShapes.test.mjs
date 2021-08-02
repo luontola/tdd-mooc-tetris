5c4558d72d2f684e2fd8530d6bdced1206d79358
Esko Luontola <esko@luontola.fi>
2021-08-02 20:40:27 +0300

    Refactor

--- test/RotatingShapes.test.mjs
+++ test/RotatingShapes.test.mjs
@@ -1,5 +1,13 @@
 import {expect} from "chai";
 
+function newSquareArray(size) {
+  const array = new Array(size);
+  for (let row = 0; row < size; row++) {
+    array[row] = new Array(size);
+  }
+  return array;
+}
+
 class RotatingShape {
   #shape;
 
@@ -9,9 +17,9 @@ class RotatingShape {
         .replaceAll(" ", "")
         .trim()
         .split("\n")
-        .map(row => row.split(""))
+        .map(row => row.split(""));
     } else {
-      this.#shape = shape
+      this.#shape = shape;
     }
   }
 
@@ -22,23 +30,11 @@ class RotatingShape {
   }
 
   rotateRight() {
-    /*
-      row,col  => row,col
-      0,0         0,2
-      0,1         1,2
-      0,2         2,2
-      1,0         0,1
-      1,1         1,1
-      1,2         2,1
-     */
-    const dimensions = this.#shape.length;
-    const rotated = new Array(dimensions);
-    for (let row = 0; row < dimensions; row++) {
-      rotated[row] = new Array(dimensions);
-    }
-    for (let row = 0; row < dimensions; row++) {
-      for (let col = 0; col < dimensions; col++) {
-        rotated[col][dimensions - 1 - row] = this.#shape[row][col];
+    const size = this.#shape.length;
+    const rotated = newSquareArray(size);
+    for (let row = 0; row < size; row++) {
+      for (let col = 0; col < size; col++) {
+        rotated[col][size - 1 - row] = this.#shape[row][col];
       }
     }
     return new RotatingShape(rotated);
@@ -77,6 +73,46 @@ describe("Rotating 3x3 shape", () => {
       `CFI
        BEH
        ADG`
+    );
+  })
+})
+
+describe("Rotating 5x5 shape", () => {
+  const shape = new RotatingShape(
+    `ABCDE
+     FGHIJ
+     KLMNO
+     PQRST
+     UVWXY`
+  );
+
+  it("initial orientation", () => {
+    expect(shape.toString()).to.equalShape(
+      `ABCDE
+       FGHIJ
+       KLMNO
+       PQRST
+       UVWXY`
+    );
+  })
+
+  it("can be rotated right/clockwise", () => {
+    expect(shape.rotateRight().toString()).to.equalShape(
+      `UPKFA
+       VQLGB
+       WRMHC
+       XSNID
+       YTOJE`
+    );
+  })
+
+  it("can be rotated left/counter-clockwise", () => {
+    expect(shape.rotateLeft().toString()).to.equalShape(
+      `EJOTY
+       DINSX
+       CHMRW
+       BGLQV
+       AFKPU`
     );
   })
 })
