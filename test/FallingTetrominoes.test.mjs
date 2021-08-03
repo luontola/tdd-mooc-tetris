224d7be6d97e09dfa1cd619f47f630d4dfdbf125
Esko Luontola <esko@luontola.fi>
2021-08-03 20:33:25 +0300

    WIP: The dropped tetromino starts from top middle

---
+++ test/FallingTetrominoes.test.mjs
@@ -0,0 +1,22 @@
+import {expect} from "chai";
+import {Board} from "../src/Board.mjs";
+import {Tetromino} from "../src/Tetromino.mjs";
+
+describe("Falling tetrominoes", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
+
+  xit("The dropped tetromino starts from top middle", () => {
+    board.drop(Tetromino.T_SHAPE);
+    expect(board.toString()).to.equalShape(
+      `....T.....
+       ...TTT....
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
+})
