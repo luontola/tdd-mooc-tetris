31aa7412bbfa453cabe260954f17a7ea0f828119
fil7er <fil7er@gmail.com>
2022-06-09 10:59:10 -0400

    uncomment

--- test/RotatingShapes.test.mjs
+++ test/RotatingShapes.test.mjs
@@ -17,7 +17,7 @@ describe("Rotating 3x3 shape", () => {
     );
   });
 
-  xit("can be rotated right/clockwise", () => {
+  it("can be rotated right/clockwise", () => {
     expect(shape.rotateRight().toString()).to.equalShape(
       `GDA
        HEB
