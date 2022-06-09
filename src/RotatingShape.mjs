fdf91dbf282ab8e3b810269a54455d3fdead1def
fil7er <fil7er@gmail.com>
2022-06-09 10:08:23 -0400

    pass

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,3 +1,11 @@
 export class RotatingShape{
-    
+    shape;
+
+    constructor(shape){
+        this.shape = shape;
+    }
+
+    toString(){
+        return this.shape.replaceAll(" ", "").trim() + "\n";
+    }
 }
