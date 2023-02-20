bbe5c82b71fb69df6879d8768111309bd1916a2f
Esko Luontola <esko@luontola.fi>
2023-02-20 17:06:00 +0200

    Draw game elements

--- src/index.mjs
+++ src/index.mjs
@@ -4,14 +4,51 @@ import { ShuffleBag } from "./ShuffleBag.mjs";
 import { Tetromino } from "./Tetromino.mjs";
 
 function drawBackground(ctx, canvasWidth, canvasHeight) {
-  ctx.fillStyle = "#FFFFFF";
+  ctx.fillStyle = "#ffffff";
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 }
 
-function drawText(ctx, { text, x, y, font }) {
+function drawText(ctx, { text, x, y, font, textAlign }) {
   ctx.font = font;
+  ctx.textAlign = textAlign || "left";
   ctx.fillStyle = "#000000";
   ctx.fillText(text, x, y);
+}
+
+const CELL_COLORS = {
+  ".": "#ffffff",
+  I: "#cc1c19",
+  T: "#3a88b2",
+  L: "#c85c14",
+  J: "#312cc3",
+  S: "#921392",
+  Z: "#2e9915",
+  O: "#9a8016",
+};
+
+function drawCell(ctx, { cell, row, column, cellWidth, cellHeight }) {
+  ctx.fillStyle = CELL_COLORS[cell];
+  const x = cellWidth * column;
+  const y = cellHeight * row;
+  ctx.fillRect(x, y, cellWidth, cellHeight);
+}
+
+function drawScoring(ctx, { score, level, canvasWidth }) {
+  const margin = 5;
+  const fontSize = 22;
+  drawText(ctx, {
+    text: `Level ${level}`,
+    x: margin,
+    y: fontSize + margin,
+    font: `${fontSize}px sans-serif`,
+  });
+  drawText(ctx, {
+    text: `Score ${score}`,
+    textAlign: "right",
+    x: canvasWidth - margin,
+    y: fontSize + margin,
+    font: `${fontSize}px sans-serif`,
+  });
 }
 
 function renderGame(game, canvas, timestamp) {
@@ -22,17 +59,22 @@ function renderGame(game, canvas, timestamp) {
   const cellHeight = canvasHeight / game.rows;
 
   drawBackground(ctx, canvasWidth, canvasHeight);
-  drawText(ctx, {
-    text: "Hello world",
-    x: 100,
-    y: 300,
-    font: "30px sans-serif",
+  for (let row = 0; row < game.rows; row++) {
+    for (let column = 0; column < game.columns; column++) {
+      const cell = game.board.cellAt(row, column);
+      drawCell(ctx, { cell, row, column, cellWidth, cellHeight });
+    }
+  }
+  drawScoring(ctx, {
+    level: game.scoring.level,
+    score: game.scoring.score,
+    canvasWidth,
   });
 }
 
 function progressTime(game, timestamp) {
   if (timestamp >= game.nextTick) {
-    if (game.board.hasFalling()) {
+    if (!game.board.hasFalling()) {
       game.board.drop(game.tetrominoes.draw());
     } else {
       game.board.tick();
@@ -47,11 +89,11 @@ function initGame() {
   const game = {
     columns: 10,
     rows: 20,
-    tickDuration: 100,
+    tickDuration: 300,
     nextTick: 0,
   };
   game.board = new Board(game.columns, game.rows);
-  game.score = new ScoringSystem();
+  game.scoring = new ScoringSystem();
   game.tetrominoes = new ShuffleBag();
   game.tetrominoes.add(Tetromino.I_SHAPE, 1);
   game.tetrominoes.add(Tetromino.T_SHAPE, 1);
@@ -62,7 +104,7 @@ function initGame() {
   game.tetrominoes.add(Tetromino.Z_SHAPE, 1);
   game.tetrominoes.add(Tetromino.O_SHAPE, 1);
   game.board.onClearLine = (lineCount) => {
-    game.score.increment(lineCount);
+    game.scoring.increment(lineCount);
   };
 
   document.addEventListener("keydown", (event) => {
